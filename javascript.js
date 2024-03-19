73e46025c495d8eecee3dcfa66f4c05a09ca93f0
joao969 <joao_marcos3@hotmail.com>
2024-03-19 20:43:03 -0300

    att2

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const sr= ScrollReveal({
+    distance :'10%',
+    duration: 2000,
+    delay: 5,
+    reset:true
+  });
+  
+  sr.reveal('h1',{delay:200, origin:'top'});
+  sr.reveal('h2',{delay:200, origin:'top'});
+  sr.reveal('h3',{delay:200, origin:'top'});
+  sr.reveal('.servico',{delay:200, origin:'top'});
+  sr.reveal('.contato',{delay:200, origin:'top'});
+  sr.reveal('.logo',{delay:200, origin:'top'});
+  sr.reveal('i',{delay:200, origin:'top'});
+  
+  
